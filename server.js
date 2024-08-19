dc1fc18d129d2704de53eac52d1dda0ef0b3a952
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-08-19 18:51:24 +0300

    server base file initiation

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({origin:process.env.CLIENT_URL , credentials:true}))
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
+
+
+
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server is running on port: ", PORT);
+});
