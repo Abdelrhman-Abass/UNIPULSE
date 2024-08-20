b35d7331af2e05ebf7bee48425f28804b7b9146a
Abdelrahman_Abass <abdoabass400@gmail.com>
2024-08-20 22:40:55 +0300

    Verification functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -19,28 +19,35 @@ export const register = async (req, res) => {
 		console.log("userAlreadyExists", userAlreadyExists);
 
 		if (userAlreadyExists) {
+            console.log(userAlreadyExists)
 			return res.status(400).json({ success: false, message: "User already exists" });
 		}
 
 
         // HASH THE PASSWORD AND MAKE A NEW VERIFICATION CODE
         const hashPassword = bcryptjs.hashSync(password, 12);
-        const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+        const VerificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+        const VerificationCodeExpires = new Date(Date.now() + 30 * 60 * 1000); // OTP valid for 30 minutes
 
-        // CREATE A NEW USER AND SAVE IT TO DB
         const newUser = await prisma.user.create({
             data: {
-                username, email, password: hashPassword,
+                username,
+                email,
+                password: hashPassword,
+                VerificationCode,
+                VerificationCodeExpires,
+                isVerified:false,
             }
-        })
+        });
+        // await sendEmailVerificationCode(email , VerificationCode.)
 
-        console.log(newUser)
-        res.status(201).json({ massage: "User created successfully" })
+        console.log("New User ID  :  " + newUser.id)
+        res.status(201).json( newUser)
 
     } catch (error) {
         console.log(error.massage)
 
-        res.status(500).json({ massage: `Failed to create user =>  ${error}` })
+        res.status(500).json({ massage: `Failed to create user =>  ${error.massage}` })
     }
 }
 
@@ -94,6 +101,101 @@ export const login = async (req, res) => {
     }
 };
 
+
+export const verifyCode = async (req, res) => {
+    const { email } = req.params;
+    const { verificationCode } = req.body;
+
+    console.log(email, typeof(verificationCode))
+
+    try {
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+        
+        console.log(user)
+        if (!user || user.isVerified) {
+            return res.status(400).json({ message: "Invalid request." });
+        }
+
+        if (user.verificationCodeExpires < new Date()) {
+            return res.status(400).json({ message: "expired verificationCode." });
+        }
+
+        if (user.VerificationCode != verificationCode ) {
+            return res.status(400).json({ message: "Invalid verificationCode." });
+        }
+
+        await prisma.user.update({
+            where: { email },
+            data: {
+                isVerified: true,
+                VerificationCode: null, // Clear verificationCode after verificationCode
+                VerificationCodeExpires: null,
+            }
+        });
+
+        res.status(200).json({ message: "Account verified successfully." });
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({ message: `Failed to verify OTP => ${error.message}` });
+    }
+};
+
+
+export const updateVerificationCode = async (req, res) => {
+    const { email } = req.body;
+
+    try {
+        // CHECK IF THE USER EXISTS
+        const user = await prisma.user.findUnique({
+            where: { email },
+        });
+
+        if (!user) {
+            return res.status(404).json({ message: "User not found!" });
+        }
+
+        if (user.isVerified) {
+            return res.status(400).json({ message: "User is already verified." });
+        }
+
+        // CREATE A NEW VERIFICATION CODE
+        const newVerificationCode = Math.floor(100000 + Math.random() * 900000).toString();
+        const newVerificationCodeExpires = new Date(Date.now() + 30 * 60 * 1000); // OTP valid for 30 minutes
+
+        // UPDATE THE USER RECORD WITH THE NEW VERIFICATION CODE
+        await prisma.user.update({
+            where: { email },
+            data: {
+                VerificationCode: newVerificationCode,
+                VerificationCodeExpires: newVerificationCodeExpires,
+            },
+        });
+
+        // await sendEmailVerificationCode(email , VerificationCode.)
+
+        res.status(200).json({ message: "New verification code has been sent." });
+
+    } catch (error) {
+        console.error("Error remaking verification code:", error);
+        res.status(500).json({ message: `Failed to remake verification code: ${error.message}` });
+    }
+};
+
 export const logout = (req, res) => {
     res.clearCookie("access_token").status(200).json({ message: "Logout Successful" });
 };
+
+export const getUsers = async (req, res, next) => {
+    try {
+        const users = await prisma.user.findMany()
+
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+
+        res.status(500).json({ message: "Failed to get users" });
+    }
+}
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,10 +1,17 @@
 import express from 'express'
-import { login, logout, register } from '../controllers/auth.controller.js'
+import { getUsers, login, logout, register, updateVerificationCode, verifyCode } from '../controllers/auth.controller.js'
 
 const router = express.Router()
 
 router.post('/register', register)
 router.post('/login', login )
 router.post('/logout',logout )
+router.post('/verify/:email',verifyCode )
+router.post('/update-verify',updateVerificationCode )
+
+router.get('/get-users', getUsers)
+
+
+
 
 export default router
